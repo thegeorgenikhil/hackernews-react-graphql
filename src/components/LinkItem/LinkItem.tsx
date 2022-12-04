aa7eea9501190a115a176f8f8e7942c3db61455c
Nikhil George <thegeorgenikhil@gmail.com>
2022-12-04 16:36:16 +0530

    styles: unused styles

--- src/components/LinkItem/LinkItem.tsx
+++ src/components/LinkItem/LinkItem.tsx
@@ -22,13 +22,15 @@ export const LinkItem: React.FC<LinkItemProps> = ({
   return (
     <div className="link-item-container">
       <p className="link-id">{id}</p>
-      <div className="link-content-container">
+      <div>
         <div className="link-content">
           <a target={"_blank"} rel={"noreferrer"} href={url}>
             <p className="link-title">{title}</p>
           </a>
           <a target={"_blank"} rel={"noreferrer"} href={url}>
-            <small className="link-url">({removeHTTPFromUrl(shortenUrl(url))})</small>
+            <small className="link-url">
+              ({removeHTTPFromUrl(shortenUrl(url))})
+            </small>
           </a>
         </div>
         <p className="link-author">by {username}</p>
